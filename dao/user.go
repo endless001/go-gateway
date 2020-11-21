d4f1fd813be08c9ca85c1e4163ee8a1647cb1d34
lq <Administrator@LST-RD-SFM>
2020-11-21 11:39:14 +0800

    update  checkuser

--- dao/user.go
+++ dao/user.go
@@ -7,14 +7,14 @@ import (
 	"go-gateway/ent/user"
 )
 
-func (d *Dao) CheckUser(c *gin.Context, userName string) (*ent.User,error)  {
-	user,err := d.client.User.Query().
-		Where(user.UsernameEQ(userName),user.IsDeleteEQ(0)).
+func (d *Dao) CheckUser(c *gin.Context, userName string, password string) (*ent.User, error) {
+	user, err := d.client.User.Query().
+		Where(user.UsernameEQ(userName), user.IsDeleteEQ(0)).
 		Only(c)
-	if err !=nil{
-		
+	if err != nil {
+
 	}
 }
-func (d *Dao) FindUser(c context.Context) (*ent.User,error) {
-	return nil,nil
+func (d *Dao) FindUser(c context.Context) (*ent.User, error) {
+	return nil, nil
 }
