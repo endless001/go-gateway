ce2b53dfdc7cc1972b79c168863697c996f235ea
lq <Administrator@LST-RD-SFM>
2020-11-28 11:14:50 +0800

    update

--- internal/server/http/oauth.go
+++ internal/server/http/oauth.go
@@ -2,62 +2,60 @@ package http
 
 import (
 	"encoding/base64"
-
-	//"errors"
-	//"github.com/dgrijalva/jwt-go"
-	//"github.com/e421083458/go_gateway/golang_common/lib"
-	//"github.com/e421083458/go_gateway/public"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
-	//"go-gateway/middleware"
+	"go-gateway/internal/middleware"
+	"go-gateway/internal/util"
 	"strings"
-	//"time"
+	"time"
 )
 
 func Tokens(c *gin.Context) {
 
 	splits := strings.Split(c.GetHeader("Authorization"), " ")
 	if len(splits) != 2 {
-		//middleware.ResponseError(c, 2000, errors.New("用户名或密码格式错误"))
+		middleware.ResponseError(c, 2000, errors.New("用户名或密码格式错误"))
 		return
 	}
 	appSecret, err := base64.StdEncoding.DecodeString(splits[1])
 	if err != nil {
-		//middleware.ResponseError(c, 20002, err)
+		middleware.ResponseError(c, 20002, err)
 	}
 	parts := strings.Split(string(appSecret), ":")
 	if len(parts) != 2 {
-		//middleware.ResponseError(c, 2003, errors.New("用户名或密码格式错误"))
+		middleware.ResponseError(c, 2003, errors.New("用户名或密码格式错误"))
 	}
-	appInfo, err := d.GetApp(c, parts[0], parts[1])
+	appInfo, err := d.GetTenant(c, parts[0], parts[1])
 	if err != nil {
-		//middleware.ResponseError(c, 2004, err)
+		middleware.ResponseError(c, 2004, err)
 		return
 	}
 	if appInfo != nil {
 
-		/**
+		localtime, err := time.LoadLocation("")
+
 		claims := jwt.StandardClaims{
 			Issuer:    appInfo.AppID,
-			ExpiresAt: time.Now().Add(public.JwtExpires * time.Second).In(lib.TimeLocation).Unix(),
+			ExpiresAt: time.Now().Add(util.JwtExpires * time.Second).In(localtime).Unix(),
 		}
-		*/
 		if err != nil {
-			//middleware.ResponseError(c, 2004, err)
+			middleware.ResponseError(c, 2004, err)
 			return
 		}
 
-		//middleware.ResponseSuccess(c, claims)
+		middleware.ResponseSuccess(c, claims)
 		return
 	}
-	//middleware.ResponseError(c, 2005, errors.New("未匹配正确APP信息"))
+	middleware.ResponseError(c, 2005, errors.New("未匹配正确APP信息"))
 }
 
 func Signout(c *gin.Context) {
 
 	sess := sessions.Default(c)
-	//	sess.Delete(public.AdminSessionInfoKey)
+	sess.Delete(util.AdminSessionInfoKey)
 	sess.Save()
-	//middleware.ResponseSuccess(c, "ok")
+	middleware.ResponseSuccess(c, "ok")
 
 }
--- internal/server/http/user.go
+++ internal/server/http/user.go
@@ -3,10 +3,11 @@ package http
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/e421083458/go_gateway/public"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"go-gateway/internal/ent"
+	"go-gateway/internal/middleware"
+	"go-gateway/internal/util"
 )
 
 func UserLogin(c *gin.Context) {
@@ -15,19 +16,18 @@ func UserLogin(c *gin.Context) {
 
 func UserInfo(c *gin.Context) {
 	sess := sessions.Default(c)
-	sessInfo := sess.Get(public.AdminSessionInfoKey)
+	sessInfo := sess.Get(util.AdminSessionInfoKey)
 	userInfo := &ent.User{}
 	if err := json.Unmarshal([]byte(fmt.Sprint(sessInfo)), userInfo); err != nil {
-		//middleware.ResponseError(c, 2000, err)
+		middleware.ResponseError(c, 2000, err)
 		return
 	}
-
-	//middleware.ResponseSuccess(c, userInfo)
+	middleware.ResponseSuccess(c, userInfo)
 }
 
 func UserSignout(c *gin.Context) {
 	sess := sessions.Default(c)
-	sess.Delete(public.AdminSessionInfoKey)
+	sess.Delete(util.AdminSessionInfoKey)
 	sess.Save()
-	//middleware.ResponseSuccess(c, "ok")
+	middleware.ResponseSuccess(c, "ok")
 }
